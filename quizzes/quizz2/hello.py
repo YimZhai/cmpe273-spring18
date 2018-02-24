8a567c0ea56da1789ab90450385cda5253561343
Yiming Zhai <yimingzhaius@gmail.com>
2018-02-23 16:41:53 -0800

    no message

--- quizzes/quizz2/hello.py
+++ quizzes/quizz2/hello.py
@@ -1,8 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
 app = Flask(__name__)
 
-@app.route("/")
+num = 1
+userList = {}
+
+@app.route('/')
 def hello():
     return "Hello World!"
 
 
+@app.route('/users', methods=['POST'])
+def add_user():
+    global num
+    users = {
+        "id": num,
+        "name": request.form["name"]
+    }
+    userList[str(num)] = request.form["name"]
+    num = num+ + 1
+    return jsonify(users), 201
+
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    if id in userList:
+        users = {
+        "id": id,
+        "name": userList[id]
+        }
+        return jsonify(users)
+    else:
+        return "User doesn't exist.", 204
+    
+           
+@app.route("/users/<id>", methods=["DELETE"])
+def delete_user(id):
+    del userList[id]
+    return "", 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
